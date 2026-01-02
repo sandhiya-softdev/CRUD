98ce5cd8c3f5969a0180fb2b4c69d5161b3e8530
sandhiyasoftdev <sandhiya.softdev@gmail.com>
2026-01-02 22:45:12 +0530

    CRUD js commit

--- put/put.js
+++ put/put.js
@@ -1,9 +1,17 @@
-function putData(){
+let things = document.getElementById("name").value;
+let things1 = document.getElementById("result");
+function putData() {
   fetch("https://jsonplaceholder.typicode.com/users/1", {
     method: "PUT",
-    headers:{
-        "Content-Type":"application/json"
+    headers: {
+      "Content-Type": "application/json",
     },
-
-    
-})
+    body: JSON.stringify({ name: things }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      things1.innerText = "updatated name" + data.name;
+    });
+}
